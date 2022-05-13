bf1151932527381d4c1c7805d07fd867e34bfd44
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-05-14 00:12:55 +0300

    Commit

--- tasks/088_sort_&_reverse_nums_array.py
+++ tasks/088_sort_&_reverse_nums_array.py
@@ -2,13 +2,15 @@
 # целых чисел и сохраните их в массиве.
 # Отсортируйте список и выведите его содержимое в обратном порядке.
 from array import *
-array = ('i', [])
 
-# prompt = int(input("How many numbers would you like to use? "))
-for i in range(0, 5):
+numArray = array ("i", [])
+prompt = int(input("How many numbers would you like to use? "))
+
+while prompt != 0:
     num = int(input("Enter a number: "))
-    array.append(num)
+    numArray.append(num)
+    prompt -= 1
 
-array = sorted(array)
-array.reverse()
-print(array)
+numArray = sorted(numArray)
+numArray.reverse()
+print(numArray)
