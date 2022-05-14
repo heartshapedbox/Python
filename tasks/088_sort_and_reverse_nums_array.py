cee74b2133b16b83adc2678ec21fb2e961a4d6c3
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-05-14 14:29:22 +0300

    Commit

--- tasks/088_sort_and_reverse_nums_array.py
+++ tasks/088_sort_and_reverse_nums_array.py
@@ -3,14 +3,14 @@
 # Отсортируйте список и выведите его содержимое в обратном порядке.
 from array import *
 
-numArray = array ("i", [])
+num_array = array ("i", [])
 prompt = int(input("How many numbers would you like to use? "))
 
 while prompt != 0:
     num = int(input("Enter a number: "))
-    numArray.append(num)
+    num_array.append(num)
     prompt -= 1
 
-numArray = sorted(numArray)
-numArray.reverse()
-print(numArray)
+num_array = sorted(num_array)
+num_array.reverse()
+print(num_array)
