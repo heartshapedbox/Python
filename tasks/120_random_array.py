e362981e0dbf2892b433920f2715af86de652ae4
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-05-16 11:01:43 +0300

    Commit

---
+++ tasks/120_random_array.py
@@ -0,0 +1,28 @@
+# Создайте массив, содержащий
+# пять рандомных чисел. Если в массиве есть повторяющиеся числа,
+# выведите это число, сколько раз оно повтояется и сам массив (или все числа массива).
+# Если в массиве нет повторяющихся чисел - генерируйте новый
+# массив с рандомными числами.
+
+from array import *
+import random
+
+result = False
+while result == False:
+    nums = array ("i", [])
+    for i in range(0, 5):
+        num = random.randint(0, 10)
+        nums.append(num)
+
+    rangeStart = 1
+    length = len(nums)
+
+    for i in range(0, length):
+        if nums[i] in nums[rangeStart:length]:
+            print(", ".join(str(i) for i in nums))
+            print(f"Number {nums[i]} is repeated {nums.count(nums[i])} times.")
+            result = True
+            break
+        else:
+            rangeStart += 1
+        result = False
