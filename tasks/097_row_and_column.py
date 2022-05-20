3f7f9ce96d8f72693c7d870e973a8721de5c250c
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-05-20 16:18:31 +0300

    Commit

---
+++ tasks/097_row_and_column.py
@@ -0,0 +1,15 @@
+# Создайте двумерный список
+# со стандартными индексами Python:
+# предложите пользователю выбрать строку и столбец и выведите выбранное значение.
+
+import  random
+
+list = []
+for i in range(0, 4):
+    nums_array = [random.randint(0, 10) for i in range(0, 3)]
+    list.append(nums_array)
+print(list)
+
+row = int(input("Please, pick a row: "))
+column = int(input("Please, pick a column: "))
+print(list[row][column])
