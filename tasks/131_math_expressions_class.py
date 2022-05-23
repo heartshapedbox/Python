ef877315bbdd9edf2c6952beeb6061809273ba5a
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-05-23 18:27:21 +0300

    Commit

--- tasks/131_math_expressions_class.py
+++ tasks/131_math_expressions_class.py
@@ -24,19 +24,13 @@ class Math:
                 result /= self.args[i]
                 self.result = round(result, 2)
         else:
-            print("Error.")
+            print("Error!")
             self.result = None
         print(f'Random list: {self.args}.')
         print(f'The result of {action} is: {self.result}.\n')
 
-arguments1 = Math([random.randrange(1, 10) for i in range(0, random.randrange(4, 7))])
-arguments1.do("subtraction")
-
-arguments2 = Math([random.randrange(1, 10) for i in range(0, random.randrange(4, 6))])
-arguments2.do("division")
-
-arguments3 = Math([random.randrange(1, 10) for i in range(0, random.randrange(4, 6))])
-arguments3.do("addition")
-
-arguments4 = Math([random.randrange(1, 10) for i in range(0, random.randrange(4, 6))])
-arguments4.do("multiplication")
+list = Math([random.randrange(1, 10) for i in range(0, random.randrange(4, 7))])
+list.do("subtraction")
+list.do("division")
+list.do("addition")
+list.do("multiplication")
