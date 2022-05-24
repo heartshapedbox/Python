1c43544aa48edd87e71a032c5da8d22aa2aeac52
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-05-24 15:21:04 +0300

    Commmit

--- tasks/100_nested_dicts.py
+++ tasks/100_nested_dicts.py
@@ -6,16 +6,11 @@
 # Anna   5239 4802 5820 1859
 # Fiona  3904 3645 8821 2451
 
-managers = ['John','Tom','Anna','Fiona']
-region = ['N','S','E','W']
-data = [[3056,8463,8441,2694],[4832,6786,4737,3612],[5239,4802,5820,1859],[3904,3645,8821,2451]]
+nested_dict = {
+'John': {'N': 3056,'S': 8463,'E': 8441,'W': 2694},
+'Tom': {'N': 4832,'S': 6786,'E': 4737,'W': 3612},
+'Anna': {'N': 5239,'S': 4802,'E': 5820,'W': 1859},
+'Fiona': {'N': 3904,'S': 3645,'E': 8821,'W': 2451}
+}
 
-nested_dict = {}
-step = 0
-for i in range(0, len(data)):
-    for i in range(0, len(data)):
-        sub_dict = {}
-        sub_dict[region[step]] = data[i][step]
-        nested_dict[managers[i]] = sub_dict
-    step += 1
-    print(nested_dict)
+print(nested_dict)
