1d96ec3c92c601b1a59a338c9543f1482b7c15fb
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-05-24 18:20:25 +0300

    Commit

---
+++ tasks/102_persons_data.py
@@ -0,0 +1,14 @@
+# Предложите пользователю ввести имя, возраст и размер обуви для четырех человек. Запросите имя одного из них в списке и выведите значения его возраста и размера обуви.
+
+print('Please, provide data about 4 persons.')
+dict = {}
+for i in range(0 ,4):
+    print(f'{i + 1} person:')
+    name = input("Please, enter a person's name: ")
+    age = int(input("Please, enter a person's age: "))
+    shoes_size = int(input("Please, enter a person's shoes size: "))
+    dict[name] = {"Age": age, "Shoes Size": shoes_size}
+print(f'Dictionary:\n {dict}')
+
+name = input("\nPlease, pick a name from the dictionary: ")
+print(f"{name}'s data:\n {dict[name]}")
