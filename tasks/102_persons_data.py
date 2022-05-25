9a8da05098836d253baeea5f181cf1979106144b
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-05-25 12:09:30 +0300

    Commit

--- tasks/102_persons_data.py
+++ tasks/102_persons_data.py
@@ -2,9 +2,9 @@
 
 print('Please, provide data about 4 persons.')
 dict = {}
-for i in range(0 ,4):
+for i in range(0, 4):
     print(f'{i + 1} person:')
-    name = input("Please, enter a person's name: ")
+    name = input("Please, enter a person's name: ").title()
     age = int(input("Please, enter a person's age: "))
     shoes_size = int(input("Please, enter a person's shoes size: "))
     dict[name] = {"Age": age, "Shoes Size": shoes_size}
