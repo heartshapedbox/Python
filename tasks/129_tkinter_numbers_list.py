f0a9efe56febd986d9cb6db0451026a28e1016e4
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-06-29 12:12:06 +0300

    Commit

---
+++ tasks/129_tkinter_numbers_list.py
@@ -0,0 +1,42 @@
+# Создайте окно, которое предлагает пользователю ввести число в текстовом поле. При нажатии кнопки используйте конструкцию varialble.isdigit() для проверки того, является ли число целым. Если проверка дает положительный результат, оно добавляется в список; в противном случае содержимое текстового поля стирается. Создайте дополнительную кнопку для очистки списка.
+
+from os import remove
+from tkinter import *
+
+from numpy import number
+
+root = Tk()
+root.title('Numbers')
+root.geometry('300x200')
+root.resizable(False, False)
+
+numbersList = []
+
+
+def add():
+    i = input.get()
+    if i.isdigit():
+        numbersList.append(i)
+    output['text'] = ','.join(i for i in numbersList)
+    input.delete(0, END)
+    
+    
+def reset():
+    while len(numbersList) > 0:
+        for i in numbersList:
+            numbersList.remove(i)
+        output['text'] = ''
+
+
+lbl = Label(root, text = 'Enter a number: ')
+lbl.pack()
+input = Entry(root, width = 16)
+input.pack()
+input.focus()
+addToListButton = Button(root, text = 'Add', bg = '#3c3c3c', fg = '#ffffff', command = add)
+addToListButton.place(x = 100, y = 50, width = 50, height = 50)
+cleanListButton = Button(root, text = "Reset", bg = '#ff4d4d', fg = '#ffffff', command = reset)
+cleanListButton.place(x = 150, y = 50, width = 50, height = 50)
+output = Message(root, text = '', width = 225)
+output.place(x = 25, y = 100)
+root.mainloop()
--- tasks/file.py
+++
@@ -1,6 +0,0 @@
-parentsList = ['Distance: Kilometer > Meter','Distance: Kilometer > Mile','Weight: Kilo > Gram','Weight: Kilo > Pound']
-parentsDict = {}
-for i in range(0, len(parentsList)):
-    parentsDict[i + 1] = parentsList[i]
-    
-print(parentsDict)
