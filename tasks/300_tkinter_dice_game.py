5a840929c57c926e4b7c61ef7d9fb86079189dc4
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-07-07 10:39:02 +0300

    Commit

--- tasks/300_tkinter_dice_game.py
+++ tasks/300_tkinter_dice_game.py
@@ -4,7 +4,7 @@ import random
 window = Tk()
 window.title('Dice Game')
 window.geometry('500x300')
-window['bg'] = 'black'
+window['bg'] = '#000000'
 
 list = ['\u2680','\u2681','\u2682','\u2683','\u2684','\u2685']
 
@@ -13,13 +13,13 @@ lbl = Label(master = window, text = ' ', font = ("Consolas", 150))
 def roll():
     lbl['text'] = f'{random.choice(list)}{random.choice(list)}'
     lbl.pack()
-    lbl['bg'] = 'black'
-    lbl['fg'] = 'white'
+    lbl['bg'] = '#000000'
+    lbl['fg'] = '#FFFFFF'
 
 
 btn = Button(master = window, font = ("Consolas", 10), text = 'Roll the Dice', command = roll, padx = 10, pady = 5)
 btn.pack()
-btn['bg'] = 'orange'
-btn['fg'] = 'black'
+btn['bg'] = '#ff4d4d'
+btn['fg'] = '#000000'
 
 window.mainloop()
