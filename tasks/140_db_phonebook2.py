f564ccb1305e187e305d52cd034e63aa6c0e1b30
Babenko Dmitry <babenko.dmitry.mail@gmail.com>
2022-08-03 18:41:29 +0300

    Commit

--- tasks/140_db_phonebook2.py
+++ tasks/140_db_phonebook2.py
@@ -8,6 +8,7 @@
 # Enter your selection: Если пользователь выбирает пункт 1, он сможет просмотреть всю телефонную книгу. Если он выбирает пункт 2, он сможет добавить новую запись в телефонную книгу. Если выбран пункт 3, программа предлагает ввести фамилию, а затем выводит записи всех людей с заданной фамилией. При выборе пункта 4 программа предлагает ввести идентификатор и удаляет соответствующую запись из таблицы. При выборе пункта 5 программа завершается.
 
 from tkinter import *
+from threading import Timer
 import customtkinter
 import sqlite3
 import os
@@ -18,13 +19,14 @@ customtkinter.set_default_color_theme('dark-blue')
 class App():
     def __init__(self):
         self.accent_color1 = '#ededed'
-        self.accent_color2 = '#2e4f87'
+        self.accent_color2 = '#3b65ad'
         self.accent_color3 = '#608bd5'
         self.accent_color4 = '#f878b6'
         self.accent_color5 = '#d6478d'
+        self.accent_color6 = '#000000'
         
         self.root = customtkinter.CTk()
-        self.root.title('Phone numbers')
+        self.root.title('Phone Book')
         x = int(self.root.winfo_screenwidth() // 2.5)
         y = int(self.root.winfo_screenheight() * 0.2)
         self.root.geometry(f'400x400+{x}+{y}')
@@ -35,14 +37,14 @@ class App():
         self.root.mainloop()
     
     def show_menu(self):
-        self.btn_frame = Frame(self.root, background = self.accent_color1)
-        self.btn_frame.pack(pady = 70)
+        self.menu_frame = Frame(self.root, background = self.accent_color1)
+        self.menu_frame.pack(pady = 70)
         
-        self.btn1 = customtkinter.CTkButton(self.btn_frame, text = 'View phone book', cursor = 'hand2', width = 200, command = lambda:self.get_numbers_from_db())
-        self.btn2 = customtkinter.CTkButton(self.btn_frame, text = 'Add to phone book', cursor = 'hand2', width = 200)
-        self.btn3 = customtkinter.CTkButton(self.btn_frame, text = 'Search for surname', cursor = 'hand2', width = 200)
-        self.btn4 = customtkinter.CTkButton(self.btn_frame, text = 'Delete person from phone book', cursor = 'hand2', width = 200)
-        self.btn5 = customtkinter.CTkButton(self.btn_frame, text = 'Quit', cursor = 'hand2', width = 200)
+        self.btn1 = customtkinter.CTkButton(self.menu_frame, text = 'View phone book', cursor = 'hand2', width = 200, command = lambda:self.show_db())
+        self.btn2 = customtkinter.CTkButton(self.menu_frame, text = 'Search for surname', cursor = 'hand2', width = 200, command = lambda:self.show_search_for_surname())
+        self.btn3 = customtkinter.CTkButton(self.menu_frame, text = 'Add to phone book', cursor = 'hand2', width = 200, command = lambda:self.show_add_number_to_db())
+        self.btn4 = customtkinter.CTkButton(self.menu_frame, text = 'Delete person from phone book', cursor = 'hand2', width = 200, command = lambda:self.show_remove_number_from_db())
+        self.btn5 = customtkinter.CTkButton(self.menu_frame, text = 'Quit', cursor = 'hand2', width = 200, command = lambda:quit())
         
         for i in (self.btn1, self.btn2, self.btn3, self.btn4):
             i.configure(
@@ -70,12 +72,8 @@ class App():
         btn.bind("<Leave>", func = lambda i: btn.configure(text_color = colorfgOnLeave)) 
     
     
-    def get_numbers_from_db(self):
-        self.btn_frame.destroy()
-        self.db_frame = Frame(self.root, background = self.accent_color1)
-        self.db_frame.pack(pady = 40)
-        self.label = customtkinter.CTkLabel(self.db_frame, text = '')
-        self.btn_back = customtkinter.CTkButton(self.root, text = '<', command = lambda:self.back())
+    def show_back_button(self):
+        self.btn_back = customtkinter.CTkButton(self.root, text = 'Back To Menu', cursor = 'hand2', width = 200, command = lambda:self.back())
         self.btn_back.configure(
             fg_color = self.accent_color2,
             hover_color = self.accent_color3,
@@ -83,25 +81,220 @@ class App():
             corner_radius = 6
         )
         self.btn_hover(self.btn_back, self.accent_color2, self.accent_color1)
-        self.btn_back.place(x = 30, y = 30, width = 30, height = 30)
+        self.btn_back.pack(pady = 2, ipadx = 2, ipady = 5)
+    
+    
+    def show_add_button(self):
+        self.btn_add = customtkinter.CTkButton(self.root, text = 'Add', cursor = 'hand2', width = 200, command = lambda:self.add())
+        self.btn_add.configure(
+            fg_color = self.accent_color5,
+            hover_color = self.accent_color4,
+            text_color = self.accent_color1,
+            corner_radius = 6
+        )
+        self.btn_hover(self.btn_add, self.accent_color5, self.accent_color1)
+        self.btn_add.pack(pady = 2, ipadx = 2, ipady = 5)
         
+        
+    def show_search_button(self):
+        self.btn_search = customtkinter.CTkButton(self.root, text = 'Search', cursor = 'hand2', width = 200, command = lambda:self.search())
+        self.btn_search.configure(
+            fg_color = self.accent_color5,
+            hover_color = self.accent_color4,
+            text_color = self.accent_color1,
+            corner_radius = 6
+        )
+        self.btn_hover(self.btn_search, self.accent_color5, self.accent_color1)
+        self.btn_search.pack(pady = 2, ipadx = 2, ipady = 5)
+        
+        
+    def show_remove_button(self):
+        self.btn_remove = customtkinter.CTkButton(self.root, text = 'Remove', cursor = 'hand2', width = 200, command = lambda:self.remove())
+        self.btn_remove.configure(
+            fg_color = self.accent_color5,
+            hover_color = self.accent_color4,
+            text_color = self.accent_color1,
+            corner_radius = 6
+        )
+        self.btn_hover(self.btn_remove, self.accent_color5, self.accent_color1)
+        self.btn_remove.pack(pady = 2, ipadx = 2, ipady = 5)
+    
+    
+    def show_db(self):
+        self.menu_frame.destroy()
+        self.page_name = 'show_db_frame'
+        self.db_frame = Frame(self.root, background = self.accent_color1)
+        self.db_frame.pack(pady = 30)
+        self.label = customtkinter.CTkLabel(self.db_frame, text = '')
         tmp = []
         db = sqlite3.connect('PhoneBook.db')
         cursor = db.cursor()
         cursor.execute("""SELECT * FROM Names""")
+        
         for i in cursor.fetchall():
-            tmp.append(f'{i[0]}: {i[1]} {i[2]}, Phone: {i[3]}')
+            tmp.append(f'{i[0]}:   {i[1]} {i[2]},   Phone Number:   {i[3]}')
             
         self.label.configure(
-            text = '\n\n'.join(str(i) for i in tmp),
+            text = '\n'.join(str(i) for i in tmp),
             justify = LEFT,
             bg_color = self.accent_color1
             )
         self.label.pack()
+        self.show_back_button()
+        
+    
+    def show_add_number_to_db(self):
+        self.menu_frame.destroy()
+        self.page_name = 'show_add_number_frame'
+        self.new_number_frame = Frame(self.root, background = self.accent_color1)
+        self.new_number_frame.pack(pady = 30)
+        
+        self.name_label = customtkinter.CTkLabel(self.new_number_frame, anchor = 'e', text = 'Enter a name:   ')
+        self.name_label.grid(row = 0, column = 0, pady = 2)
+        self.surname_label = customtkinter.CTkLabel(self.new_number_frame, anchor = 'e', text = 'Enter a surname:   ')
+        self.surname_label.grid(row = 1, column = 0, pady = 2)
+        self.number_label = customtkinter.CTkLabel(self.new_number_frame, anchor = 'e', text = 'Enter a phone number:   ')
+        self.number_label.grid(row = 2, column = 0, pady = 2)
+        
+        self.name_entry = customtkinter.CTkEntry(self.new_number_frame, border_width = 0, corner_radius = 6)
+        self.name_entry.focus()
+        self.name_entry.grid(row = 0, column = 1)
+        self.surname_entry = customtkinter.CTkEntry(self.new_number_frame, border_width = 0, corner_radius = 6)
+        self.surname_entry.grid(row = 1, column = 1)
+        self.number_entry = customtkinter.CTkEntry(self.new_number_frame, border_width = 0, corner_radius = 6)
+        self.number_entry.grid(row = 2, column = 1)
+        
+        self.message = customtkinter.CTkLabel(self.new_number_frame, anchor = 's', text = '')
+        self.message.grid(row = 4, columnspan = 2, ipady = 10)
+        self.show_add_button()
+        self.show_back_button()
+        
+        
+    def show_search_for_surname(self):
+        self.menu_frame.destroy()
+        self.page_name = 'show_search_for_surname_frame'
+        self.search_for_surname_frame = Frame(self.root, background = self.accent_color1)
+        self.search_for_surname_frame.pack(pady = 30)
+        
+        self.search_for_surname = customtkinter.CTkLabel(self.search_for_surname_frame, anchor = 'e', text = 'Enter a surname:   ')
+        self.search_for_surname.grid(row = 0, column = 0, pady = 2)
+        
+        self.search_for_surname_entry = customtkinter.CTkEntry(self.search_for_surname_frame, border_width = 0, corner_radius = 6)
+        self.search_for_surname_entry.focus()
+        self.search_for_surname_entry.grid(row = 0, column = 1)
+        
+        self.message = customtkinter.CTkLabel(self.search_for_surname_frame, anchor = 's', text = '')
+        self.message.grid(row = 4, columnspan = 2, ipady = 10)
+        self.show_search_button()
+        self.show_back_button()
+        
+        
+    def show_remove_number_from_db(self):
+        self.menu_frame.destroy()
+        self.page_name = 'show_remove_number_from_db'
+        self.remove_number_from_db_frame = Frame(self.root, background = self.accent_color1)
+        self.remove_number_from_db_frame.pack(pady = 30)
+        
+        self.search_for_id = customtkinter.CTkLabel(self.remove_number_from_db_frame, anchor = 'e', text = 'Enter an ID:   ')
+        self.search_for_id.grid(row = 0, column = 0, pady = 2)
+        
+        self.search_for_id_entry = customtkinter.CTkEntry(self.remove_number_from_db_frame, border_width = 0, corner_radius = 6)
+        self.search_for_id_entry.focus()
+        self.search_for_id_entry.grid(row = 0, column = 1)
+        
+        self.message = customtkinter.CTkLabel(self.remove_number_from_db_frame, anchor = 's', text = '')
+        self.message.grid(row = 4, columnspan = 2, ipady = 10)
+        self.show_remove_button()
+        self.show_back_button()
+    
+    
+    def add(self):
+        self.name = self.name_entry.get()
+        self.surname = self.surname_entry.get()
+        self.number = self.number_entry.get()
+        db = sqlite3.connect('PhoneBook.db')
+        cursor = db.cursor()
+        
+        if len(self.name) > 0 and len(self.surname) > 0 and len(self.number) > 0:
+            cursor.execute("""INSERT INTO Names(FirstName, Surname, PhoneNumber) VALUES(?,?,?)""", (self.name, self.surname, self.number))
+            db.commit()
+            self.message.configure(text = 'Added!', text_color = self.accent_color2)
+            Timer(1.5, self.remove_message).start()
+        else:
+            self.message.configure(text = 'Please, fill all the fields!', text_color = self.accent_color5)
+            Timer(1.5, self.remove_message).start()
+            
+        db.close()
+        self.name_entry.delete(0, END)
+        self.surname_entry.delete(0, END)
+        self.number_entry.delete(0, END)
+        
+        
+    def search(self):
+        self.surname = self.search_for_surname_entry.get()
+        tmp = []
+        db = sqlite3.connect('PhoneBook.db')
+        cursor = db.cursor()
+        
+        if len(self.surname) > 0:
+            cursor.execute(f"""SELECT * FROM Names WHERE Surname = '{self.surname}' """)
+            
+            for i in cursor.fetchall():
+                tmp.append(f'{i[1]} {i[2]},   Phone Number:   {i[3]}')
+                db.commit()
+                
+            if len(tmp) > 0:
+                self.message.configure(text = '\n'.join(str(i) for i in tmp), text_color = self.accent_color6, justify = LEFT)
+            else:
+                self.message.configure(text = 'No matches.', text_color = self.accent_color5, justify = LEFT)
+                Timer(1.5, self.remove_message).start()
+        else:
+            self.message.configure(text = 'Please, fill the field!', text_color = self.accent_color5)
+            Timer(1.5, self.remove_message).start()
+            
+        db.close()
+        self.search_for_surname_entry.delete(0, END)
+        
+    
+    def remove(self):
+        self.id = self.search_for_id_entry.get()
+        tmp = []
+        db = sqlite3.connect('PhoneBook.db')
+        cursor = db.cursor()
+        cursor.execute("""SELECT * FROM Names""")
+        
+        for i in cursor.fetchall():
+            tmp.append(f'{i[0]}')
+            
+        if self.id in tmp:
+            cursor.execute(f"""DELETE FROM Names WHERE ID = '{self.id}' """)
+            db.commit()
+            self.message.configure(text = 'Removed!', text_color = self.accent_color2, justify = LEFT)
+            Timer(1.5, self.remove_message).start()
+        else:
+            self.message.configure(text = 'No matches.', text_color = self.accent_color5, justify = LEFT)
+            Timer(1.5, self.remove_message).start()
+            
+        db.close()
+        self.search_for_id_entry.delete(0, END)
+    
+    
+    def remove_message(self):
+        self.message.configure(text = '')
     
     
     def back(self):
-        self.db_frame.destroy()
+        if self.page_name == 'show_db_frame':
+            self.db_frame.destroy()
+        elif self.page_name == 'show_add_number_frame':
+            self.new_number_frame.destroy()
+            self.btn_add.destroy()
+        elif self.page_name == 'show_search_for_surname_frame':
+            self.search_for_surname_frame.destroy()
+            self.btn_search.destroy()
+        else:
+            self.remove_number_from_db_frame.destroy()
+            self.btn_remove.destroy()
         self.btn_back.destroy()
         self.show_menu()
 
